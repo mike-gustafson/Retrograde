6dfba2170b924acd0168940eaa502529e129a107
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-12-02 08:58:59 -0800

    feat

--- controllers/platforms.js
+++ controllers/platforms.js
@@ -5,10 +5,13 @@ const sortData = require('../utils/sort-data.util');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 async function generatePlatformHTML(platform, platformLogos) {
+  const platformLogos2 = await PlatformLogo.findAll();
+
+    // Log the fetched platformLogos
+    console.log('Platform Logos:', platformLogos2[0].url);
   const logoUrl = (platformLogos.find(logo => logo.id === platform.platformLogo)?.url.replace('jpg',
     'png').replace('t_thumb', 't_720p') || '//techterms.com/img/sm/cd_290.png');
-console.log(platformLogos.find(logo => logo.id === platform.platformLogo)?.url.replace('jpg',
-'png').replace('t_thumb', 't_720p'));
+
   return `
     <div class="col mb-1">
       <div class="p-1 h-100">
